cbcba7c18188b96117b904e367232ffd085d1b89
Srihari <ssvz@visishta.net>
2012-03-10 13:11:26 +0530

    only tito

--- app/dialogs/tito-dialog.h
+++ app/dialogs/tito-dialog.h
@@ -14,7 +14,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 #ifndef __TITO_DIALOG_H__
 #define __TITO_DIALOG_H__
 
