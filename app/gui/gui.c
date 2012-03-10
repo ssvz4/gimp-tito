c633881fd5681dc8ac43a653d30fa46aa53e7922
Srihari <ssvz@visishta.net>
2012-03-10 11:46:44 +0530

    added tito

--- app/gui/gui.c
+++ app/gui/gui.c
@@ -492,6 +492,10 @@ gui_restore_after_callback (Gimp               *gimp,
                          "/dummy-menubar/image-popup/Help/dialogs-about",
                          _("About GIMP"));
 
+    gui_add_to_app_menu (image_ui_manager, group,
+                         "/dummy-menubar/image-popup/Help/dialogs-tito",
+                         _("About GIMP"));
+
     /*  the preferences group  */
     group = ige_mac_menu_add_app_menu_group ();
 
